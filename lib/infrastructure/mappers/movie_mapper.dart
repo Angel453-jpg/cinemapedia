ea2caef10d39618c2f33cb886a0df5d15100a2d2
Angel <angelin09ozoz@gmail.com>
2026-01-06 20:33:04 -0600

    Continuación de la app cinemapedia peliculas individuales y actores

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,25 +1,49 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/movie_db/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/movie_db/movie_movie_db.dart';
 
 class MovieMapper {
-  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
-    adult: movieDB.adult,
-    backdropPath: (movieDB.backdropPath != '')
-        ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
-        : 'https://cdn.displate.com/artwork/857x1200/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
-    genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
-    id: movieDB.id,
-    originalLanguage: movieDB.originalLanguage,
-    originalTitle: movieDB.originalTitle,
-    overview: movieDB.overview,
-    popularity: movieDB.popularity * 1000,
-    posterPath: (movieDB.posterPath != '')
-        ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
-        : 'no-poster',
-    releaseDate: movieDB.releaseDate,
-    title: movieDB.title,
-    video: movieDB.video,
-    voteAverage: movieDB.voteAverage,
-    voteCount: movieDB.voteCount,
-  );
+  static Movie movieDBToEntity(MovieMovieDB movieDB) =>
+      Movie(
+        adult: movieDB.adult,
+        backdropPath: (movieDB.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+            : 'https://cdn.displate.com/artwork/857x1200/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+        genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+        id: movieDB.id,
+        originalLanguage: movieDB.originalLanguage,
+        originalTitle: movieDB.originalTitle,
+        overview: movieDB.overview,
+        popularity: movieDB.popularity * 1000,
+        posterPath: (movieDB.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+            : 'no-poster',
+        releaseDate: movieDB.releaseDate,
+        title: movieDB.title,
+        video: movieDB.video,
+        voteAverage: movieDB.voteAverage,
+        voteCount: movieDB.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movieDB) =>
+      Movie(
+            adult: movieDB.adult,
+            backdropPath: (movieDB.backdropPath != '')
+                ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+                : 'https://cdn.displate.com/artwork/857x1200/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            genreIds: movieDB.genres.map((e) => e.name).toList(),
+            id: movieDB.id,
+            originalLanguage: movieDB.originalLanguage,
+            originalTitle: movieDB.originalTitle,
+            overview: movieDB.overview,
+            popularity: movieDB.popularity * 1000,
+            posterPath: (movieDB.posterPath != '')
+                ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+                : 'no-poster',
+            releaseDate: movieDB.releaseDate,
+            title: movieDB.title,
+            video: movieDB.video,
+            voteAverage: movieDB.voteAverage,
+            voteCount: movieDB.voteCount,
+      );
 }
